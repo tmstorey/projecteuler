7d99235300b8abc89c04903d074d4ca6cf60690e
Thomas Storey <tom@storey.id.au>
2013-08-31 17:39:15 +1000

    solution to problem 7

--- 7.py
+++ 7.py
@@ -3,3 +3,9 @@
 # https://projecteuler.net/problem=7
 # What is the 10,001st prime number?
 
+from primes import generate
+
+for n, prime in enumerate(generate()):
+    if n == 10000:
+        print prime
+        break
